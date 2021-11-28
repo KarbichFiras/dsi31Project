b139fb082fd80a7185dbb755133baa23f2a06ac7
KarbichFiras <firaskarbich@gmail.com>
2021-11-28 21:05:31 +0100

    linked login component with navbar

--- FornEnd/Angular/src/app/authentication/authentication-routing.module.ts
+++ FornEnd/Angular/src/app/authentication/authentication-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: AuthenticationComponent }
+  { path : "login", component : LoginComponent},
+  //{ path : '', component: AuthenticationComponent },
+  { path: '',   redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
