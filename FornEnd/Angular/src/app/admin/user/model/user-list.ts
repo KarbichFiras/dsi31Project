44f74892cf839b8257cd6c8c95df8fa6689497ec
ihebessaied <i.essaied@yahoo.fr>
2021-11-29 01:07:06 +0100

    add user model and list  user interface to admin

---
+++ FornEnd/Angular/src/app/admin/user/model/user-list.ts
@@ -0,0 +1,22 @@
+import { user } from "./user-model";
+        export const users: user[] = [
+            {
+                id:"fsgsdgsdg687",
+                nom:"ihrb",
+                prenom:"essaied",
+                date_naissance:"06/06/2265",
+                adresse:"qsfqfqsefeqdfq",
+                tel:"9882101",
+            },
+        
+            {
+                id:"fsgsdgsdg681",
+                nom:"sxfds",
+                prenom:"kar98",
+                date_naissance:"06/06/2456",
+                adresse:"srgsdtg",
+                tel:"9882101",
+            },
+        ]
+   
+
