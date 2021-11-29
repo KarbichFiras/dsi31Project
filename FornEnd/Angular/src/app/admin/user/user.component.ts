bf4b3a817816df347438be649f41089570b416f1
ihebessaied <i.essaied@yahoo.fr>
2021-11-29 20:41:05 +0100

    add read all user

--- FornEnd/Angular/src/app/admin/user/user.component.ts
+++ FornEnd/Angular/src/app/admin/user/user.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Users } from '../user/model/user-list';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
+users = Users ;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  getUesrs():typeof Users{
+    return this.users;
+  }
 }
