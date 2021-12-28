e324dff15c09c9876d1f053aa48f07f02cc5ecc3
KarbichFiras <firaskarbich@gmail.com>
2021-12-28 10:40:13 +0100

    succesfully send post request

--- FornEnd/Angular/src/app/authentication/singnup/singnup.component.ts
+++ FornEnd/Angular/src/app/authentication/singnup/singnup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, NgForm, NgModel } from '@angular/forms';
+import{  SignupService } from '../../Services/Apis/signup.service';
 @Component({
   selector: 'app-singnup',
   templateUrl: './singnup.component.html',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingnupComponent implements OnInit {
 
-  constructor() { }
+  userForm;
+
+  constructor(private signupService:SignupService) { }
 
   ngOnInit(): void {
+      
+  }
+
+  signUp(){
+
+    console.log("sign u p method tregered !")
+    
+    const postBody={
+      username : "testUsername",
+      email : "testEmail",
+      password : "12345"
+    }
+
+    this.signupService.addUser(postBody).subscribe(
+      (data)=>{
+        if(data == null){
+          console.log("Failed to sign you up! please try again later");
+        }else{
+          console.log("Signed up succesfuly");
+        }
+      },
+      (error)=>{
+        //ma7abetch tcatchi lerror
+        console.log("Failed to sign you up! please try again later");
+      }
+    );
+
   }
 
 }
