b10fc16767f1992f579ccedc56d716d42e9c4d05
ihebessaied <i.essaied@yahoo.fr>
2021-12-31 02:51:03 +0100

    add service food

--- FornEnd/Angular/src/app/food/food.component.ts
+++ FornEnd/Angular/src/app/food/food.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { FoodService } from '../Services/Apis/food.service';
 @Component({
   selector: 'app-food',
   templateUrl: './food.component.html',
@@ -8,13 +8,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FoodComponent implements OnInit {
   restaurantCode : string ="";
-  constructor(private route: ActivatedRoute) { }
+  foodExtraCateg;
+  constructor(private route: ActivatedRoute,private foodService:FoodService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params=>{
       this.restaurantCode = params.id;
       
       //console.log(this.restaurantCode);
+      // this.foodService.getffods().subscribe(data=>{
+      //   this.foodExtraCateg=data;
+      // })
     })
   }
 
