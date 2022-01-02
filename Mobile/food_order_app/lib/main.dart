3f9079ee55fcce148cd0a69cf7fe4a1413f1a655
layes-git <80711416+layes-git@users.noreply.github.com>
2022-01-02 07:18:06 +0100

    create project flutter customize pubspec.yaml+customize appbar

---
+++ Mobile/food_order_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+      child: MaterialApp(
+        title: 'wakalni',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          accentColor: Colors.orange,
+          fontFamily: 'Lato',
+        ),
+        //home: ,
+        
+    );
+
+  }
+}
