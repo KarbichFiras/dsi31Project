4fae7ba4a9acdf79321b4385722f5a904b56b92f
KarbichFiras <firaskarbich@gmail.com>
2022-01-02 16:34:14 +0100

    create appdrwer view+implement routing and providers in main+create food and foods models

--- Mobile/food_order_app/lib/main.dart
+++ Mobile/food_order_app/lib/main.dart
@@ -1,39 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:food_order_app/providers/cart.dart';
-import 'package:food_order_app/screens/cart_screen.dart';
+
 import 'package:food_order_app/screens/login.dart';
 import 'package:food_order_app/screens/register.dart';
-import 'package:food_order_app/screens/restaurant_overview_screen.dart';
-import 'package:food_order_app/widgets/FoodDetailScreen.dart';
+
 import 'package:provider/provider.dart';
-import './screens/foods_overview_screen.dart';
+
 import './providers/foods.dart';
-import './providers/restaurants.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return 
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Foods()),
+      ],
       child: MaterialApp(
-        title: 'chmin9lews',
-        theme: ThemeData(
-          primarySwatch: Colors.red,
-          accentColor: Colors.orange,
-          fontFamily: 'Lato',
-        ),
-        home: Login(),
-        routes: {
-          Login.routeName: (ctx) => Login(),
-          Register.routeName: (ctx) => Register(),
-          RestauarantsOverviewScreen.routeName: (ctx) => RestauarantsOverviewScreen(),
-          FoodDetailScreen.routeName: (ctx) => FoodDetailScreen(),
-          CartScreen.routeName: (ctx) => CartScreen(),
-        }
-        ),
+          title: 'chmin9lews',
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            accentColor: Colors.orange,
+            fontFamily: 'Lato',
+          ),
+          home: Login(),
+          routes: {
+            Login.routeName: (ctx) => Login(),
+            Register.routeName: (ctx) => Register()
+          }),
     );
-
   }
 }
